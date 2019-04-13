56c842137e62216f6853448d2986d3bd8907aa56
Jakub Cieciala <jakub.cieciala@gmail.com>
2019-04-13 17:04:49 +0200

    init

---
+++ app/src/main/java/temperature/kuba/tempera/MainActivity.java
@@ -0,0 +1,31 @@
+package temperature.kuba.tempera;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_widget_alarm_manager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_widget_alarm_manager, menu);
+        return true;
+    }
+}
